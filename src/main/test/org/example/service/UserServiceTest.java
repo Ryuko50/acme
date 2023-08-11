7966c1c81105ac178d36df66fee1f8425bcf7fbc
JuanFe <juanfelipe3010@yahoo.com>
2023-08-11 13:27:55 +0200

    tests final

--- src/main/test/org/example/service/UserServiceTest.java
+++ src/main/test/org/example/service/UserServiceTest.java
@@ -19,6 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,5 +51,79 @@ public class UserServiceTest {
         when(userRepository.findById(any())).thenReturn(Optional.empty());
 
         assertThrows(Exception.class, () -> userService.getUser(2L));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldSaveUserWhenUserNoExists(){
+        User mockUser = new User();
+        UserDto mockUserDto = UserDto.builder().name("pepito").password("123").build();
+
+        when(modelMapper.map(any(), any())).thenReturn(mockUser);
+        when(userRepository.save(any())).thenReturn(mockUser);
+
+        userService.saveOrUpdate(mockUserDto);
+
+        verify(userRepository).save(any());
+        verify(modelMapper).map(any(),any());
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldThrowExceptionWhenIdExists(){
+        User mockUser = new User();
+        UserDto mockUserDto = UserDto.builder().id(2L).name("pepito").password("123").build();
+
+        when(userRepository.existsById(any())).thenReturn(false);
+
+
+        assertThrows(Exception.class, () -> userService.saveOrUpdate(mockUserDto));
+
+
+
+        verify(userRepository).existsById(any());
+
+
+
+
+
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldUpdateUserWhenIdExists() {
+        User mockUser = new User();
+        UserDto mockUserDto = UserDto.builder().id(2L).name("pepito").password("123").build();
+
+        when(userRepository.existsById(any())).thenReturn(true);
+        when(modelMapper.map(any(), any())).thenReturn(mockUser);
+        when(userRepository.save(any())).thenReturn(mockUser);
+
+
+        userService.saveOrUpdate(mockUserDto);
+
+
+        verify(userRepository).existsById(any());
+        verify(modelMapper).map(any(),any());
+        verify(userRepository).save(any());
+
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldDeleteUserWhenIdExists(){
+        when(userRepository.existsById(any())).thenReturn(true);
+
+        userService.deleteUser(1L);
+
+        verify(userRepository).existsById(any());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNoExists(){
+        when(userRepository.existsById(any())).thenReturn(false);
+
+        assertThrows(Exception.class, () -> userService.deleteUser(2L));
+
     }
 }
